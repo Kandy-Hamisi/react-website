b6232330cee971ea83d6476300583d1ac03b94c9
Kandy-Hamisi <hkandy2000@gmail.com>
2021-07-29 14:46:37 +0300

    worked on the hero-section component to completeness

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact></Route>
+          <Route path="/" exact component={Home}></Route>
         </Switch>
       </Router>
     </>
