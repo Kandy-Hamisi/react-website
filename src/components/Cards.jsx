f9fa96ed25b5394f098da13632ec3cae36a8ac22
Kandy-Hamisi <hkandy2000@gmail.com>
2021-07-29 15:28:36 +0300

    working on the card component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CardItem from './Carditem';
+import './Cards.css';
+
 function Cards() {
     return (
         <div className="cards">
@@ -7,7 +9,12 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                            src="images/img-9.jpg"
+                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                            label="Adventure"
+                            path="/services"
+                        />
                     </ul>
                 </div>
             </div>
