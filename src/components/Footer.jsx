0fa2e7ec001506f6b08a75b24dfd5ef5168dea01
Kandy-Hamisi <hkandy2000@gmail.com>
2021-07-30 09:29:36 +0300

    finished up with the footer component and the entire  website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                 <p className="footer-subscription-text">You can subscribe at any time</p>
                 <div className="input-areas">
                     <form>
-                        <input type="email" name="email" className="footer-input"></input>
+                        <input type="email" name="email" className="footer-input" placeholder="Your Email"></input>
                         <Button buttonStyle="btn--outline">Subscribe</Button>
                     </form>
                 </div>
@@ -22,12 +22,67 @@ function Footer() {
                     <div className="footer-link-items">
                         <h2>About Us</h2>
                         <Link to="/sign-up">How it Works</Link>
+                        <Link to="/">Testimonials</Link>
+                        <Link to="/">Careers</Link>
+                        <Link to="/">Investors</Link>
+                        <Link to="/">Terms of Service</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Contact Us</h2>
+                        <Link to="/sign-up">How it Works</Link>
+                        <Link to="/">Testimonials</Link>
+                        <Link to="/">Careers</Link>
+                        <Link to="/">Investors</Link>
+                        <Link to="/">Terms of Service</Link>
+                    </div>
+                </div>
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>Videos</h2>
+                        <Link to="/sign-up">How it Works</Link>
+                        <Link to="/">Testimonials</Link>
+                        <Link to="/">Careers</Link>
+                        <Link to="/">Investors</Link>
+                        <Link to="/">Terms of Service</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Social Media</h2>
+                        <Link to="/sign-up">How it Works</Link>
+                        <Link to="/">Testimonials</Link>
+                        <Link to="/">Careers</Link>
+                        <Link to="/">Investors</Link>
+                        <Link to="/">Terms of Service</Link>
                     </div>
                 </div>
             </div>
+
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link className="social-logo" to="/">
+                            TRVL <i className="fab fa-typo3"></i>
+                        </Link>
+                    </div>
+                    <small className="website-rights">TRVL &copy; 2020</small>
+                    <div className="social-icons">
+                        <Link className="social-icon-link facebook" to="/" target="_blank" aria-label="Facebook">
+                            <i className="fab fa-facebook-f"></i>
+                        </Link>
+                        <Link className="social-icon-link instagram" to="/" target="_blank" aria-label="Instagram">
+                            <i className="fab fa-instagram"></i>
+                        </Link>
+                        <Link className="social-icon-link twitter" to="/" target="_blank" aria-label="Twitter">
+                            <i className="fab fa-twitter"></i>
+                        </Link>
+                        <Link className="social-icon-link linkedin" to="/" target="_blank" aria-label="LinkedIn">
+                            <i className="fab fa-linkedin"></i>
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
 
 
-export default Footer
+export default Footer;
